7e5d76efe3d4697cb14093e1f15f96da132b1759
Ömer Said Bulduk <omer@kintshop.com>
2023-04-17 16:08:46 +0300

    pages, templates added

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>INDEX SAYFASI</h1>")
+    return render(request, 'index.html')
 
 
+def about(request):
+    return render(request, 'about.html')
