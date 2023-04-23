9196c2541caf4e62faa3ed076540a65d581bae6d
omersb <omersaidbuldukk@gmail.com>
2023-04-23 18:54:04 +0300

    accounts eklendi tempaltes kısmı güncellendi

---
+++ smartedu_con/accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+def user_login(request):
+    pass
+
+
+def user_register(request):
+    pass
+
+
+def user_dashboard(request):
+    pass
+
+
+def user_logout(request):
+    pass
+
